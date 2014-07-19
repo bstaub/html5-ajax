7d19a677eddbbded5a930a342da2f3c16700c0b4
bstaub <bruno.staub@intersolution.ch>
2014-07-19 19:19:35 +0200

    fix ohne Content-Type: text/xml !

---
+++ weatherforcast/getweather.php
@@ -0,0 +1,15 @@
+<?php
+	// Datei getweather.php
+//	header("content-type: text/xml");
+	$w = $_GET['w'];
+	$u = $_GET['u'];
+//	$url = "http://weather.yahooapis.com/forecastrss?w=$w&u=$u";
+	$url = "http://weather.yahooapis.com/forecastrss?w=23707368&u=c";
+ 
+	$xml=file_get_contents($url);
+	if ( $xml == false ) {echo "<error>Could not read weather data from weather.yahooapis.com.</error>";
+	} else {
+		echo "$xml";
+	}
+ 
+?>
