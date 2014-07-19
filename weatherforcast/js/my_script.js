831ca72925d0c350489c308c4a0de0f6cbd9d624
bstaub <bruno.staub@intersolution.ch>
2014-07-20 00:10:15 +0200

    css added

--- weatherforcast/js/my_script.js
+++ weatherforcast/js/my_script.js
@@ -10,14 +10,18 @@ $("#submit").click(function(){
 //$.get("http://html5.stbr.ch/ajax/weatherforcast/getweather.php",function(data){
 $.get("http://localhost/weatherforcast/getweather.php?w=" + wertStandort+ "&u=" + wertEinheit +"",function(mydata){
 
-	var cond,temp,code,color;
+	var cond,temp,code,color,datum;
 //	alert("Data loaded: " + mydata);
 	cond = $('yweather\\:condition', mydata);
 	temp = cond.attr("temp");
 	code = cond.attr("code");
- 
+	datum = cond.attr("date");
+	
+	
+	$("h3").text("aktuelles Wetter");
 	$("#wetterbild").attr("src", "http://l.yimg.com/a/i/us/we/52/" + code + ".gif");
 	$("#temp").text(temp + " Grad Celsius");
+	$("#datum").text(datum);
  
 	if(temp < 0){
 		color="blue";
@@ -30,7 +34,7 @@ $.get("http://localhost/weatherforcast/getweather.php?w=" + wertStandort+ "&u=" 
 	}else{
 		color="red";
 	}
-	$("body").css("background-color",color);
+	$("#box2").css("background-color",color);
  
 });
 
