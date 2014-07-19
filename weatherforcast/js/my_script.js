7f39002a5f6e09f22e243ffb79a8d57a03c24867
bstaub <bruno.staub@intersolution.ch>
2014-07-19 19:17:53 +0200

    fix content type

--- weatherforcast/js/my_script.js
+++ weatherforcast/js/my_script.js
@@ -1,1 +1,27 @@
 
+//$.get("http://html5.stbr.ch/ajax/weatherforcast/getweather.php",function(data){
+$.get("http://localhost/weatherforcast/getweather.php",function(mydata){
+
+	var cond,temp,code,color;
+//	alert("Data loaded: " + mydata);
+	cond = $('yweather\\:condition', mydata);
+	temp = cond.attr("temp");
+	code = cond.attr("code");
+ 
+	$("#wetterbild").attr("src", "http://l.yimg.com/a/i/us/we/52/" + code + ".gif");
+	$("#temp").text(temp + " Grad Celsius");
+ 
+	if(temp < 0){
+		color="blue";
+	}else if (temp < 10){
+		color="green";
+	}else if (temp < 20){
+		color="yellow";
+	}else if (temp < 30){
+		color="orange";
+	}else{
+		color="red";
+	}
+	$("body").css("background-color",color);
+ 
+});
